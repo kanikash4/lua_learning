68c4c109fcb6258ccfa24dfe71bc872250256e57
Kanika Sharma <kanikash4@gmail.com>
2017-12-15 23:12:11 +0530

    Initial structure to use telegram bot API

---
+++ core.lua
@@ -0,0 +1,19 @@
+local api = {}
+local https = require('ssl.https')
+local multipart = require('multipart-post')
+local ltn12 = require('ltn12')
+local json = require('dkjson')
+
+function api.configure(token, debug)
+    if not token or type(token) ~= 'string' then
+        token = nil
+    end
+    api.debug = debug and true or false
+    api.token = assert(token, 'Please specify your bot API token you received from @BotFather!')
+    repeat
+        api.info = api.get_me()
+    until api.info.result
+    api.info = api.info.result
+    api.info.name = api.info.first_name
+    return api
+end
